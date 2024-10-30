472db65786d214127f258a57ee1e1bf54e888a8f
job <jobtennis21@gmail.com>
2024-10-30 14:14:25 +0300

    chore: adding shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from "path"
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
+  plugins: [react(), vike()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }
 
 export default config
+
