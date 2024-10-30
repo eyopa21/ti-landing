06c6d90f4622182d2064cd4a21c0e37b914bc457
job <jobtennis21@gmail.com>
2024-10-30 16:10:56 +0300

    feat: bottom hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { CalendarDays, DollarSign, Percent, MoveRight } from "lucide-react"
+import {
+  Card,
+  CardContent,
+  
+} from "@/components/ui/card"
 
 export default function Hero() {
   return (
@@ -24,15 +30,50 @@ export default function Hero() {
             </div>
             <div className="inline-flex  gap-4 py-8">
 
-              <Button   >
+              <Button  size={"lg"} >
                 View Your Rates
-                
+                <MoveRight />
                 </Button>
-              <Button variant={'outline'} size={"lg"} >Learn More</Button>
+              <Button variant={'outline'} size={"lg"} className="bg-transparent border-white text-white border-2" >Learn More</Button>
             </div>
           </div>
+
         </div>
+        <section className="container mx-auto px-32 py-12 absolute bottom-0">
+        <div className=" grid grid-cols-1 md:grid-cols-3 bg-white rounded-lg">
+          <Card className=" ">
+            <CardContent className="p-6 flex gap-4">
+              <Percent className="h-8 w-8 text-primary mb-4" />
+              <div>
+                <h3 className="text-xl font-bold mb-2">Low fixed rates</h3>
+                <p className="text-gray-600">Start with a low fixed-rate personal loan and you could save big.</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className=" ">
+            <CardContent className="flex gap-4 p-6">
+              <DollarSign className="h-8 w-8 text-primary mb-4" />
+              <div>
+                <h3 className="text-xl font-bold mb-2">Fast Funds, No Fuss</h3>
+                <p className="text-gray-600">Funds When You Need Them Your Quick Cash Solution!</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="">
+            <CardContent className="p-6 flex gap-4">
+              <CalendarDays className="h-8 w-8 text-primary mb-4" />
+              <div>
+                <h3 className="text-xl font-bold mb-2">Apply online</h3>
+                <p className="text-gray-600">Our online application process is quick and easy. Have your rate in just 60 seconds to get started.</p>
+              </div>
+               </CardContent>
+          </Card>
+        </div>
+      </section>
       </div>
     </>
   );
 }
+
+
+
