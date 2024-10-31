7da8a2c74a8ce599a48ecf14028bd66b4f891b2b
job <jobtennis21@gmail.com>
2024-10-31 15:40:10 +0300

    fix: good desktop  ui done

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { CalendarDays, DollarSign, Percent, MoveRight } from "lucide-react"
-import {
-  Card,
-  CardContent,
-  
-} from "@/components/ui/card"
+import { CalendarDays, DollarSign, Percent, MoveRight } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Hero() {
   return (
@@ -19,7 +15,7 @@ export default function Hero() {
         >
           <div className="mx-2 text-center  -mt-32">
             <div className="space-y-8">
-              <h1 className="text-gray-100 font-extrabold text-3xl xs:text-5xl md:text-7xl">
+              <h1 className="text-gray-100 font-extrabold text-3xl xs:text-5xl md:text-[5.5rem]">
                 <span className="text-teal-500">Fast-Track Your Finances</span>
               </h1>
               <h2 className="text-gray-200 font-extrabold text-3xl xs:text-4xl md:text-7xl leading-tight line-clamp-2">
@@ -28,52 +24,69 @@ export default function Hero() {
                 simple Online!
               </h2>
             </div>
-            <div className="inline-flex  gap-4 py-8">
-
-              <Button  size={"default"} >
+            <div className="inline-flex  gap-4 mt-20 py-8">
+              <Button size={"default"}>
                 View Your Rates
                 <MoveRight />
-                </Button>
-              <Button  variant={'outline'} size={"default"} className="bg-transparent border-white text-white border-2" >Learn More</Button>
+              </Button>
+              <Button
+                variant={"outline"}
+                size={"default"}
+                className="bg-transparent border-white text-white border-2"
+              >
+                Learn More
+              </Button>
             </div>
           </div>
-
         </div>
         <section className="container mx-auto px-32 py-12 absolute bottom-0">
-        <div className=" grid grid-cols-1 md:grid-cols-3 bg-white rounded-lg">
-          <Card className=" ">
-            <CardContent className="p-6 flex gap-4">
-              <Percent className="h-8 w-8 text-primary mb-4" />
-              <div>
-                <h3 className="text-xl font-bold mb-2">Low fixed rates</h3>
-                <p className="text-gray-600">Start with a low fixed-rate personal loan and you could save big.</p>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className=" ">
-            <CardContent className="flex gap-4 p-6">
-              <DollarSign className="h-8 w-8 text-primary mb-4" />
-              <div>
-                <h3 className="text-xl font-bold mb-2">Fast Funds, No Fuss</h3>
-                <p className="text-gray-600">Funds When You Need Them Your Quick Cash Solution!</p>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className="">
-            <CardContent className="p-6 flex gap-4">
-              <CalendarDays className="h-8 w-8 text-primary mb-4" />
-              <div>
-                <h3 className="text-xl font-bold mb-2">Apply online</h3>
-                <p className="text-gray-600">Our online application process is quick and easy. Have your rate in just 60 seconds to get started.</p>
-              </div>
-               </CardContent>
-          </Card>
-        </div>
-      </section>
+          <div className=" grid grid-cols-1 md:grid-cols-3 bg-white rounded-lg p-4">
+            <Card className="rounded-none border-none shadow-none">
+              <CardContent className="p-4 flex gap-4">
+                <div className="bg-white text-black border-black border-2 rounded-full h-16 w-16 flex items-center justify-center flex-shrink-0">
+                  <Percent className="h-8 w-8 text-primary " />
+                </div>
+                <div>
+                  <h3 className="text-xl font-bold mb-2">Low fixed rates</h3>
+                  <p className="text-gray-600">
+                    Start with a low fixed-rate personal loan and you could save
+                    big.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+            <Card className=" rounded-none border-y-0 shadow-none">
+              <CardContent className="flex gap-4 p-4 ">
+                <div className="bg-white text-black border-black border-2 rounded-full h-16 w-16 flex items-center justify-center flex-shrink-0">
+                  <DollarSign className="h-8 w-8 text-primary " />
+                </div>
+                <div>
+                  <h3 className="text-xl font-bold mb-2">
+                    Fast Funds, No Fuss
+                  </h3>
+                  <p className="text-gray-600">
+                    Funds When You Need Them Your Quick Cash Solution!
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+            <Card className="rounded-none border-none shadow-none">
+              <CardContent className="p-2 flex gap-4">
+                <div className="bg-white text-black border-black border-2 rounded-full h-16 w-16 flex items-center justify-center flex-shrink-0">
+                  <CalendarDays className="h-8 w-8 text-primary " />
+                </div>
+                <div>
+                  <h3 className="text-xl font-bold mb-2">Apply online</h3>
+                  <p className="text-gray-600">
+                    Our online application process is quick and easy. Have your
+                    rate in just 60 seconds to get started.
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </section>
       </div>
     </>
   );
 }
-
-
-
--- components/scheduleMeeting.tsx
+++ components/scheduleMeeting.tsx
@@ -1,31 +1,34 @@
 import { Button } from "@/components/ui/button";
-import { CalendarDays, ArrowRight } from "lucide-react";
+import {  ArrowRight, CalendarSearch } from "lucide-react";
 
 export default function ScheduleMeeting() {
   return (
     <>
-      <section className="bg-blue-50  mb-32 px-80">
+      <section className="bg-blue-50  mb-32 px-64 py-20">
         <div className="container mx-auto h-full w-full">
-          <div className="flex flex-row items-center justify-between gap-8">
+          <div className="grid grid-cols-5 gap-8 items-center">
            
-              <CalendarDays className="size-48 text-primary" />
-              <div className="line-clamp-3">
-                <h2 className="text-3xl font-bold mb-2">
+              <CalendarSearch className="size-32 text-primary justify-self-end" />
+              <div className="line-clamp-3 col-span-3 ">
+                <h2 className="text-4xl font-bold mb-2">
                   Schedule a Meeting Today and Discover How
 
                   <span className="text-primary font-bold">
                   &nbsp;   Hardshiprelief
                   </span>
                 </h2>
-                <p className="text-3xl">
+                <p className="text-4xl font-bold">
                   Can Transform Your Future
                 </p>
               </div>
            
             <Button className="">
-              Book a call Now <ArrowRight className="ml-2 h-4 w-4" />
+              Book a call Now <ArrowRight />
             </Button>
           </div>
+
+
+    
         </div>
       </section>
     </>
