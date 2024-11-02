167605d9c7e04f777374eeeb193cb5671967521a
job <jobtennis21@gmail.com>
2024-11-02 11:28:31 +0300

    feat: bg image with gradient

--- components/contact.tsx
+++ components/contact.tsx
@@ -60,117 +60,54 @@ export default function Contact() {
   return (
     <>
    
-      <section className="bg-black lg:bg-blue-950 py-20">
-        <div className=" px-8  lg:px-4">
-          <div className="flex justify-center ">
-            <h2 className="text-4xl md:text-5xl w-full lg:w-2/3 font-bold text-white text-left lg:text-center mb-12">
-              Get a free consultant from one of our experts right now!
-            </h2>
-          </div>
-          <div className="block lg:hidden mb-16 space-y-16">
-            <div>
-              <p className="text-primary gap-2 mb-2 font-bold flex text-2xl items-center ">
-                <Phone /> Call for inquiry
-              </p>
-              <a href="tel:1-800 557 5192" className="text-4xl text-white font-bold">
+   <section className="relative">
+      <div 
+        className="px-8 lg:px-4 py-20 bg-cover bg-center w-full"
+        style={{
+          backgroundImage: `
+            linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 128, 0.7)),
+            url('/contactBG.png')
+          `,
+        }}
+      >
+        <div className="flex justify-center">
+          <h2 className="text-4xl md:text-5xl w-full lg:w-2/3 font-bold text-white text-left lg:text-center mb-12">
+            Get a free consultant from one of our experts right now!
+          </h2>
+        </div>
+        <div className="block lg:hidden mb-16 space-y-16">
+          <div>
+            <p className="text-primary gap-2 mb-2 font-bold flex text-2xl items-center">
+              <Phone /> Call for inquiry
+            </p>
+            <a href="tel:1-800 557 5192" className="text-4xl text-white font-bold">
               1-800 557 5192
-              </a>
-            </div>
-            <div>
-              <p className="text-primary gap-2 mb-2 font-bold flex text-2xl items-center ">
-                <Mail /> Mail us for Inquiry
-              </p>
-              <a href="mailto:info@quikstartllc.com" className="text-[2.1rem] text-white font-bold">
-              info@quikstartllc.com
-              </a>
-            </div>
+            </a>
           </div>
-          <div className="max-w-5xl mx-auto space-y-6 bg-white rounded-xl p-8 py-12  lg:p-12">
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-6"
-              >
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                         <Label className="font-semibold">First & Last Name</Label>
-                        <FormControl>
-                          <Input
-                            
-                            className="bg-white"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                         <Label className="font-semibold">Email</Label>
-                        <FormControl>
-                          <Input
-                            type="email"
-                           
-                            className="bg-white"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="phone"
-                    render={({ field }) => (
-                      <FormItem>
-                         <Label className="font-semibold">Phone Number</Label>
-                        <FormControl>
-                          <Input
-                           
-                            className="bg-white"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="subject"
-                    render={({ field }) => (
-                      <FormItem>
-                         <Label className="font-semibold">Subject</Label>
-                        <FormControl>
-                          <Input
-                           
-                            className="bg-white"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+          <div>
+            <p className="text-primary gap-2 mb-2 font-bold flex text-2xl items-center">
+              <Mail /> Mail us for Inquiry
+            </p>
+            <a href="mailto:info@quikstartllc.com" className="text-[2.1rem] text-white font-bold">
+              info@quikstartllc.com
+            </a>
+          </div>
+        </div>
+        <div className="max-w-5xl mx-auto space-y-6 bg-white rounded-xl p-8 py-12 lg:p-12">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6"
+            >
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <FormField
                   control={form.control}
-                  name="message"
+                  name="name"
                   render={({ field }) => (
                     <FormItem>
-                      <Label className="font-semibold">Message</Label>
+                      <Label className="font-semibold">First & Last Name</Label>
                       <FormControl>
-                        <Textarea
-                          placeholder="Type you message......"
+                        <Input
                           className="bg-white"
                           {...field}
                         />
@@ -179,40 +116,107 @@ export default function Contact() {
                     </FormItem>
                   )}
                 />
-                <div className="flex justify-center w-full">
-                  <FormField
-                    control={form.control}
-                    name="terms"
-                    render={({ field }) => (
-                      <FormItem>
-                        <div className="flex flex-row items-center  space-x-3 ">
-                          <FormControl>
-                            <Checkbox
-                              checked={field.value}
-                              onCheckedChange={field.onChange}
-                            />
-                          </FormControl>
-                          <div className=" leading-none">
-                            <label className="text-sm lg:text-xl text-primary">
-                              I accept the privacy and terms
-                            </label>
-                          </div>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label className="font-semibold">Email</Label>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          className="bg-white"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label className="font-semibold">Phone Number</Label>
+                      <FormControl>
+                        <Input
+                          className="bg-white"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="subject"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label className="font-semibold">Subject</Label>
+                      <FormControl>
+                        <Input
+                          className="bg-white"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label className="font-semibold">Message</Label>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Type your message......"
+                        className="bg-white"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-center w-full">
+                <FormField
+                  control={form.control}
+                  name="terms"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex flex-row items-center space-x-3">
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        <div className="leading-none">
+                          <label className="text-sm lg:text-xl text-primary">
+                            I accept the privacy and terms
+                          </label>
                         </div>
-                        <div className=" text-xs px-2">
-                          <FormMessage />
-                        </div>
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <Button type="submit" className="w-full ">
-                  SUBMIT
-                </Button>
-              </form>
-            </Form>
-          </div>
+                      </div>
+                      <div className="text-xs px-2">
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                SUBMIT
+              </Button>
+            </form>
+          </Form>
         </div>
-      </section>
+      </div>
+    </section>
     </>
   );
 }
